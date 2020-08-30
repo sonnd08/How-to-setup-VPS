b65dcedde9de4d77363b2f3ba4968cbc6b36233d
sonnd08 <sonnd@dgroup.co>
2020-08-30 20:58:24 +0700

    add build version from react

---
+++ react-front-end-build/precache-manifest.15248e6785936347cd4679604b223be0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd2d55e4b3820a72479131c36ba5a3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90f789f523e0a3f9c466",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "be2bb73d2308900f6f04",
+    "url": "/static/js/2.e9a51d8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e9a51d8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90f789f523e0a3f9c466",
+    "url": "/static/js/main.16675987.chunk.js"
+  },
+  {
+    "revision": "205dba4f9959a084aa7c",
+    "url": "/static/js/runtime-main.c2a4c5c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
